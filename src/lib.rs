01f189014824bd1c70d3654160e14663c666e842
acertig <acertig04@gmail.com>
2023-02-15 21:06:41 +0100

    resolvable impls for buffer caching and stuff

--- src/lib.rs
+++ src/lib.rs
@@ -17,27 +17,6 @@ pub enum Error {
     GpuNotFound,
 }
 
-mod element {
-    /// Prevent others from implementing Element for their own types.
-    pub trait Sealed {}
-}
-
-/// Valid element types to operate on.
-pub trait Element: element::Sealed + NoUninit {}
-
-macro_rules! impl_element {
-    ($($ident:ident)+) => {$(
-        impl Element for $ident {}
-        impl element::Sealed for $ident {}
-    )+}
-}
-
-impl_element! {
-    f32 f64
-    i8 i16 i32 i64 isize
-    u8 u16 u32 u64 usize
-}
-
 pub struct Handler {
     device: wgpu::Device,
     module: wgpu::ShaderModule,
@@ -79,6 +58,28 @@ impl Handler {
     }
 }
 
+
+mod element {
+    /// Prevent others from implementing Element for their own types.
+    pub trait Sealed {}
+}
+
+/// Valid element types to operate on.
+pub trait Element: element::Sealed + NoUninit {}
+
+macro_rules! impl_element {
+    ($($ident:ident)+) => {$(
+        impl Element for $ident {}
+        impl element::Sealed for $ident {}
+    )+}
+}
+
+impl_element! {
+    f32 f64
+    i8 i16 i32 i64 isize
+    u8 u16 u32 u64 usize
+}
+
 pub enum Depth { 
     Entry,
     Bridge(u32),
@@ -95,63 +96,69 @@ pub enum Shader {
     Path(&'static str),
 }
 
+pub struct Component<T: Element, const N: usize> {
+    identity: Identity,
+    local: [T; N], 
+}
+
+pub struct Context<T: Element, const N: usize> {
+    depth: Depth,
+    shader: Shader,
+    lhs: Component<T, N>, 
+    rhs: Component<T, N>,
+}
+
+pub trait Bridge {}
+
 pub trait Resolvable {
     type Output;
+
+    fn resolve(self) -> Self::Output;
 }
 
-pub struct Component<T: Element, const N: usize> {
-    local: [T; N], 
-    identity: Identity,
-}
-
-impl<T: Element, const N: usize> Resolvable for Component<T, N> {
+impl<T: Element, const N: usize> Resolvable for [T; N] {
     type Output = Component<T, N>;
-}
 
-pub struct Context<F: Element, S: Element, const L: usize, const R: usize> {
-    shader: Shader,
-    lhs: Component<F, L>, 
-    rhs: Component<S, R>,
-    depth: Depth,
-}
-
-impl<F: Element, S: Element, const L: usize, const R: usize> Resolvable for Context<F, S, L, R> {    
-    
-}
-pub trait Bridge {}
+    fn resolve(self) -> Self::Output {
+        Component { 
+            identity: unsafe {
+                static mut ops: u32 = 0;
+                ops += 1;
+            
+                match ops {
+                    1 => Identity::Unique, 
+                    _ => {
+                        Identity::Shared(ops)
+                    }
+                }
+            },
+            local: self,
+        }
+    }
+} 
 
 macro_rules! impl_arithmetic {
     ($($fn:ident, $op:ident;)+) => {$(   
-        impl<T: Element, const L: usize, const R: usize> ops::$op for Component<T, L> {
-            pub fn $fn(self, other: Component<T, R>) -> Context<T, T, L, R> where 
-                T: ops::$op,
-            {   
 
+        impl<T: Element, const N: usize> ops::$op for Component<T, N> {        
+            type Output = Context<T, N>;
+
+            fn $fn(self, other: Component<T, N>) -> Self::Output where {  
                 Context {
-                    shader: Shader::Builtin($fn),
-                    self,
-                    other,
                     depth: Depth::Entry,
+                    shader: Shader::Builtin("$fn"),
+                    lhs: self,
+                    rhs: other,
                 }
             }
         }
 
-        impl<F: Element, S: Element, const L: usize, const R: usize> ops::$op for Component<F, L> {
-            pub fn $fn(self, other: Component<S, R>) -> Context<F, S, L, R> where
-                F: ops::$op,
-                S: ops::$op,
-            {
-                Context {
-                    shader: Shader::Builtin($fn),
-                    self,
-                    other,
-                    depth: Depth::Entry,
-                }
-            }
-        }
     )+}
 } 
 
 impl_arithmetic! {
     add, Add;
+    sub, Sub;
+    mul, Mul;
+    div, Div;
 }
